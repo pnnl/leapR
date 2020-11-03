e9c4b3aae9d91b8d63644836df1c1b797c5a5e13
Sara Gosline <sara.gosline@pnnl.gov>
2020-11-03 12:51:18 -0800

    added data.R

---
+++ R/data.R
@@ -0,0 +1,45 @@
+#' Protein expression data
+#' 
+#' A dataset containing protein expression data containing 1999 proteins and 174 patient samples. 
+#' Values represent the logratio of the protein expression compared to a control
+#' @format A data frame with 1999 rows and 174 columns
+#' @source Where did this come from?
+"protdata"
+
+#'NCI Gene lists
+#'
+#'A list of pathways and the genes that comprise these pathways
+#'@format A list with 4 items
+#'\describe{
+#' \item{names} The names of the signling pathways
+#' \item{desc}
+#' \item{sizes}
+#' \item{matrix}
+#' }
+#' @source Where did this come from?
+#'
+"ncipid"
+
+#' MSigDB Gene Lists
+#' 
+#' @format A list with 4 items
+#'\describe{
+#' \item{names} The names of the signling pathways
+#' \item{desc}
+#' \item{sizes}
+#' \item{matrix}
+#' }
+#' @source from where?
+#' 
+"msigdb"
+
+#' A list of pathways and genes that comprise these pathways from msigdb
+#' @format a list with 4 items
+#' 
+#' Short list of patient samples
+#' 
+"shortlist"
+#' Long list of patient samples
+#' 
+"longlist"
+
