00060024ed7ca08c877d3151a35d94b0728f2aad
Sara JC Gosline <sara.gosline@pnnl.gov>
2026-03-13 09:37:29 -0700

    added doc changes

--- R/enrichment_in_groups.R
+++ R/enrichment_in_groups.R
@@ -18,6 +18,7 @@
 #' to log your data before calling. NOTE: if you do not call `suppressWarnings` then
 #' the KS test will warn you about ties.
 #' @param minsize minimum size of set
+#' @param log_transformed Set to TRUE if data are log transformed
 #' @param mapping_column column name of mapping identifiers
 #' @param abundance_column columns mapping abundance, either in the `assay`
 #' matrix or `rowData`
